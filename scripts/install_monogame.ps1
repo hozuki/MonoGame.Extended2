604225c010616fae0c02a2539a0010e7bcdac300
George Wu <uiharu@buaa.edu.cn>
2023-03-08 02:59:41 +0800

    Update CI script

--- scripts/install_monogame.ps1
+++
@@ -1,7 +0,0 @@
-
-# http://community.monogame.net/t/solved-build-monogame-3-6-application-on-appveyor-ci/8840/5
-# Install MonoGame
-echo "Downloading MonoGame v3.6..."
-(New-Object Net.WebClient).DownloadFile('http://www.monogame.net/releases/v3.6/MonoGameSetup.exe', 'C:\MonoGameSetup.exe')
-echo "Installing..."
-Invoke-Command -ScriptBlock {C:\MonoGameSetup.exe /S /v/qn}
